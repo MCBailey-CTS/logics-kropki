d80aa63692fc7c052430952505a42e44e55d3b43
Mark Bailey <mcbailey@toolingsystemsgroup.com>
2022-07-17 16:48:25 -0400

    Working on Chain_Debww

--- src/abstract/BaseDiamondChain.ts
+++ src/abstract/BaseDiamondChain.ts
@@ -32,4 +32,19 @@ export abstract class BaseDiamondChain extends BaseKropkiChain {
 
     return str;
   }
+
+  // getKropkiString(puzzle:IKropkiPuzzle,chain:Loc[]):string{
+  //   let str = "";
+
+  //   for (let i = 0; i < chain.length - 1; i++) {
+  //     const loc0 = chain[i];
+  //     const loc1 = chain[i + 1];
+
+  //     str += puzzle.getCellString(puzzle.getIntersection(loc0, loc1));
+  //   }
+
+  //   str += puzzle.getCellString(puzzle.getIntersection(chain[3], chain[0]));
+
+  //   return str;
+  // }
 }
