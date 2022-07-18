2b90560e8c5fa1556ba52a2f13123e8b7e83c29a
MCBaileyGitHub <MCBAileyGitHub@gmail.com>
2022-07-18 09:35:45 -0400

    Fixed some errors

--- src/new_solvers/Chain_Dbwww.ts
+++ src/new_solvers/Chain_Dbwww.ts
@@ -4,6 +4,10 @@ import { IKropkiPuzzle } from "../interfaces/IKropkiPuzzle";
 import { BaseDiamondChain } from "../abstract/BaseDiamondChain";
 
 export class Chain_Dbwww extends BaseDiamondChain {
+  get expectedKropkiString(): string {
+    return "bwww";
+  }
+
   solve1(puzzle: IKropkiPuzzle, chain: Loc[]): IEdit[] {
     const edits: IEdit[] = [];
 
@@ -11,25 +15,5 @@ export class Chain_Dbwww extends BaseDiamondChain {
     edits.push(...this.remove(puzzle, chain[1], 4));
 
     return edits;
-  }
-
-  solve(puzzle: IKropkiPuzzle, chain: Loc[], reverse?: boolean): IEdit[] {
-    if (typeof reverse == "undefined") reverse = true;
-
-    const temp = [...chain];
-    const chain1 = [...chain];
-
-    for (let i = 0; i < chain.length; i++) {
-      if (this.getKropkiString(puzzle, chain1) == "bwww")
-        return this.solve1(puzzle, chain1);
-
-      this.pop_push(chain1);
-    }
-
-    if (!reverse) return new Array<IEdit>();
-
-    temp.reverse();
-
-    return this.solve(puzzle, temp, false);
   }
 }
--- src/new_solvers/Chain_Debww.ts
+++ src/new_solvers/Chain_Debww.ts
@@ -2,67 +2,32 @@ import { Loc } from "../Loc";
 import { IEdit } from "../interfaces/IEdit";
 import { IKropkiPuzzle } from "../interfaces/IKropkiPuzzle";
 import { BaseDiamondChain } from "../abstract/BaseDiamondChain";
-import { Edit } from "../Edit";
 
 const BLACK_WHITE = [1, 5, 7, 9];
 const WHITE_EMPTY = [3, 5, 7, 9];
 const EMPTY_BLACK = [5, 7, 9];
 const WHITE_WHITE = [1, 4, 6, 8, 9];
 
-export function pop_push(chain1: Loc[]) {
-  const item = chain1.shift();
-
-  if (typeof item == "undefined") throw Error();
-
-  chain1.push(item);
-}
-
 export class Chain_Debww extends BaseDiamondChain {
-  solve1(
-    puzzle: IKropkiPuzzle,
-    blackWhite: Loc | undefined,
-    whiteWhite: Loc | undefined,
-    whiteEmpty: Loc | undefined,
-    emptyBlack: Loc | undefined
-  ): IEdit[] {
+  get expectedKropkiString(): string {
+    return "ww.b";
+  }
+
+  solve1(puzzle: IKropkiPuzzle, chain: Loc[]): IEdit[] {
     const edits: IEdit[] = [];
 
     // black -> white
-    if (blackWhite)
-      edits.push(...this.remove(puzzle, blackWhite, ...BLACK_WHITE));
+    edits.push(...this.remove(puzzle, chain[0], ...BLACK_WHITE));
 
     // white -> white
-    if (whiteWhite)
-      edits.push(...this.remove(puzzle, whiteWhite, ...WHITE_WHITE));
+    edits.push(...this.remove(puzzle, chain[1], ...WHITE_WHITE));
 
     // white -> empty
-    if (whiteEmpty)
-      edits.push(...this.remove(puzzle, whiteEmpty, ...WHITE_EMPTY));
+    edits.push(...this.remove(puzzle, chain[2], ...WHITE_EMPTY));
 
     // empty -> black
-    if (emptyBlack)
-      edits.push(...this.remove(puzzle, emptyBlack, ...EMPTY_BLACK));
+    edits.push(...this.remove(puzzle, chain[3], ...EMPTY_BLACK));
 
     return edits;
-  }
-
-  solve(puzzle: IKropkiPuzzle, chain: Loc[], reverse?: boolean): IEdit[] {
-    if (typeof reverse == "undefined") reverse = true;
-
-    const temp = [...chain];
-    const chain1 = [...chain];
-
-    for (let i = 0; i < chain.length; i++) {
-      if (this.getKropkiString(puzzle, chain1) == "ww.b")
-        return this.solve1(puzzle, chain1[0], chain1[1], chain1[2], chain1[3]);
-
-      pop_push(chain1);
-    }
-
-    if (!reverse) return new Array<IEdit>();
-
-    temp.reverse();
-
-    return this.solve(puzzle, temp, false);
   }
 }
