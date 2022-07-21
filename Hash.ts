a04de9f87263a70070a23e9be7e9ace378ca9a7f
Mark Bailey <mcbailey@toolingsystemsgroup.com>
2022-07-20 21:27:31 -0400

    finish implementing Hash.ts

--- Hash.ts
+++ Hash.ts
@@ -1,6 +1,52 @@
 import { IHash } from "./IHash";
 
-export class Hash implements IHash 
-{
+export class Hash implements IHash {
+  private readonly __hash: Set<number>;
+
+  constructor(){
+    this.__hash = new Set<number>();
+  }
+
+  get _size(): number {
+    return this.__hash.size;
+  }
+
+  get _items(): number[] {
+    return [...this.__hash];
+  }
+
+  clear(): boolean {
+    throw new Error("Method not implemented.");
+  }
   
+  add(item: number): boolean {
+    throw new Error("Method not implemented.");
+  }
+  delete(item: number | number[]): boolean {
+    throw new Error("Method not implemented.");
+  }
+  or_union(hash: IHash): IHash {
+    throw new Error("Method not implemented.");
+  }
+  and_intersection(hash: IHash): IHash {
+    throw new Error("Method not implemented.");
+  }
+  subtract(hash: IHash): IHash {
+    throw new Error("Method not implemented.");
+  }
+  set_equals(hash: IHash): boolean {
+    throw new Error("Method not implemented.");
+  }
+  is_subset_of(hash: IHash): boolean {
+    throw new Error("Method not implemented.");
+  }
+  is_proper_subset_of(hash: IHash): boolean {
+    throw new Error("Method not implemented.");
+  }
+  is_superset_of(hash: IHash): boolean {
+    throw new Error("Method not implemented.");
+  }
+  is_proper_superset_of(hash: IHash): boolean {
+    return;
+  }
 }
