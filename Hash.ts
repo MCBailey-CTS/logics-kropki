046c69a7a277151bf118e3d0f2c315ac807d009b
Mark Bailey <mcbailey@toolingsystemsgroup.com>
2022-07-20 21:33:52 -0400

    Need to continue implementing Hash.ts

--- Hash.ts
+++ Hash.ts
@@ -3,7 +3,7 @@ import { IHash } from "./IHash";
 export class Hash implements IHash {
   private readonly __hash: Set<number>;
 
-  constructor(){
+  constructor() {
     this.__hash = new Set<number>();
   }
 
@@ -18,35 +18,44 @@ export class Hash implements IHash {
   clear(): boolean {
     throw new Error("Method not implemented.");
   }
-  
+
   add(item: number): boolean {
     throw new Error("Method not implemented.");
   }
+
   delete(item: number | number[]): boolean {
     throw new Error("Method not implemented.");
   }
+  
   or_union(hash: IHash): IHash {
     throw new Error("Method not implemented.");
   }
+
   and_intersection(hash: IHash): IHash {
     throw new Error("Method not implemented.");
   }
+
   subtract(hash: IHash): IHash {
     throw new Error("Method not implemented.");
   }
+
   set_equals(hash: IHash): boolean {
-    throw new Error("Method not implemented.");
+    return hash._size == this._size && this.is_superset_of(hash);
   }
+
   is_subset_of(hash: IHash): boolean {
     throw new Error("Method not implemented.");
   }
+  
   is_proper_subset_of(hash: IHash): boolean {
     throw new Error("Method not implemented.");
   }
+
   is_superset_of(hash: IHash): boolean {
     throw new Error("Method not implemented.");
   }
+
   is_proper_superset_of(hash: IHash): boolean {
-    return;
+    return false;
   }
 }
