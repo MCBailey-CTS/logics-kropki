2c7b492a5398f4c60762c1b01e137639ff4eb096
Mark Bailey <mcbailey@toolingsystemsgroup.com>
2022-07-24 19:58:42 -0400

    Moved most sudoku kropki techniques to only implement IKropkiSolver

--- src/solvers/sudoku/CrossHatch.ts
+++ src/solvers/sudoku/CrossHatch.ts
@@ -2,10 +2,13 @@ import { IHash } from "../../../IHash";
 import { _BaseKropkiChain } from "../../abstract/_BaseKropkiChain";
 import { Edit } from "../../Edit";
 import { IEdit } from "../../interfaces/IEdit";
+import { IKropkiChain } from "../../interfaces/IKropkiChain";
+import { IKropkiPuzzle } from "../../interfaces/IKropkiPuzzle";
+import { IKropkiSolver } from "../../interfaces/IKropkiSolver";
 import { Loc } from "../../Loc";
 import { cellCandidates } from "../../puzzles/KropkiPuzzle";
 
-export class CrossHatch extends _BaseKropkiChain {
+export class CrossHatch implements IKropkiChain {
   solve(cellChainLocs: IHash<Loc>): IEdit[] {
     const edits: IEdit[] = [];
 
@@ -64,5 +67,88 @@ export class CrossHatch extends _BaseKropkiChain {
     for (const house of this.puzzle.getHouses()) chains.push(house);
 
     return chains;
+  }
+
+  __puzzle: IKropkiPuzzle | undefined = undefined;
+
+  get puzzle(): IKropkiPuzzle {
+    if (typeof this.__puzzle == "undefined")
+      throw Error(`You have not set a puzzle for the solver ${this.id}`);
+
+    return this.__puzzle;
+  }
+
+  set puzzle(puzzle: IKropkiPuzzle) {
+    if (typeof this.__puzzle != "undefined")
+      throw Error(`You can only set a puzzle one time the solver ${this.id}`);
+
+    this.__puzzle = puzzle;
+  }
+
+  solvePuzzle(): IEdit[] {
+    const edits: IEdit[] = [];
+
+    for (const locs of this.findChains()) edits.push(...this.solve(locs));
+
+    return edits;
+  }
+
+  // abstract findChains(): IHash<Loc>[];
+
+  get id(): string {
+    return this.constructor.name;
+  }
+
+  // abstract solve(cellChainLocs: IHash<Loc>): IEdit[];
+
+  static solve(puzzle: IKropkiPuzzle, solvers: IKropkiSolver[]) {
+    const edits: IEdit[] = [];
+
+    for (const solver of solvers) solver.puzzle = puzzle;
+
+    while (true) {
+      const originalLength = edits.length;
+
+      for (const solver of solvers) edits.push(...solver.solvePuzzle());
+
+      if (originalLength == edits.length) break;
+    }
+
+    return edits;
+  }
+
+  getKropkiString(puzzle: IKropkiPuzzle, chain: IHash<Loc>): string {
+    let str = "";
+
+    for (let i = 0; i < chain._length - 1; i++) {
+      const loc0 = chain._at(i);
+      const loc1 = chain._at(i + 1);
+
+      str += puzzle.getCellString(puzzle.getIntersection(loc0, loc1));
+    }
+
+    str += puzzle.getCellString(
+      puzzle.getIntersection(chain._at(3), chain._at(0))
+    );
+
+    return str;
+  }
+
+  remove(loc: Loc, ...candidates: number[]): IEdit[] {
+    const edits: IEdit[] = [];
+
+    for (const candidate of candidates)
+      if (this.puzzle.removeCandidate(loc, candidate))
+        edits.push(new Edit(this.puzzle, loc, candidate, this));
+
+    return edits;
+  }
+
+  pop_push(chain1: IHash<Loc>) {
+    const item = chain1.shift();
+
+    if (typeof item == "undefined") throw Error();
+
+    chain1.push(item);
   }
 }
--- src/solvers/sudoku/NakedPair.ts
+++ src/solvers/sudoku/NakedPair.ts
@@ -3,10 +3,11 @@ import { _BaseKropkiChain } from "../../abstract/_BaseKropkiChain";
 import { Edit } from "../../Edit";
 import { IEdit } from "../../interfaces/IEdit";
 import { IKropkiPuzzle } from "../../interfaces/IKropkiPuzzle";
+import { IKropkiSolver } from "../../interfaces/IKropkiSolver";
 import { Loc } from "../../Loc";
 import { NewTechniques } from "../../NewTechniques";
 
-export class NakedPair extends _BaseKropkiChain {
+export class NakedPair implements IKropkiSolver {
   solve(cellChainLocs: IHash<Loc>): IEdit[] {
     const edits: IEdit[] = [];
 
@@ -29,4 +30,70 @@ export class NakedPair extends _BaseKropkiChain {
 
     return chains;
   }
+
+  //////////////
+  __puzzle: IKropkiPuzzle | undefined = undefined;
+
+  get puzzle(): IKropkiPuzzle {
+    if (typeof this.__puzzle == "undefined")
+      throw Error(`You have not set a puzzle for the solver ${this.id}`);
+
+    return this.__puzzle;
+  }
+
+  set puzzle(puzzle: IKropkiPuzzle) {
+    if (typeof this.__puzzle != "undefined")
+      throw Error(`You can only set a puzzle one time the solver ${this.id}`);
+
+    this.__puzzle = puzzle;
+  }
+
+  solvePuzzle(): IEdit[] {
+    const edits: IEdit[] = [];
+
+    for (const locs of this.findChains()) edits.push(...this.solve(locs));
+
+    return edits;
+  }
+
+  get id(): string {
+    return this.constructor.name;
+  }
+
+
+  getKropkiString(puzzle: IKropkiPuzzle, chain: IHash<Loc>): string {
+    let str = "";
+
+    for (let i = 0; i < chain._length - 1; i++) {
+      const loc0 = chain._at(i);
+      const loc1 = chain._at(i + 1);
+
+      str += puzzle.getCellString(puzzle.getIntersection(loc0, loc1));
+    }
+
+    str += puzzle.getCellString(
+      puzzle.getIntersection(chain._at(3), chain._at(0))
+    );
+
+    return str;
+  }
+
+  remove(loc: Loc, ...candidates: number[]): IEdit[] {
+    const edits: IEdit[] = [];
+
+    for (const candidate of candidates)
+      if (this.puzzle.removeCandidate(loc, candidate))
+        edits.push(new Edit(this.puzzle, loc, candidate, this));
+
+    return edits;
+  }
+
+  pop_push(chain1: IHash<Loc>) {
+    const item = chain1.shift();
+
+    if (typeof item == "undefined") throw Error();
+
+    chain1.push(item);
+  }
+  //////////
 }
